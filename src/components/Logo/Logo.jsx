2334885984c8713b9c0d9e671a9cd093b5d9c149
Mahdi Hasan <hasanmahdi6000@gmail.com>
2025-11-23 17:46:48 +0600

    Add Footer style on footer

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import logo from '../../../assets/logo.png';
 import { Link } from 'react-router';
 
-const Logo = () => {
+const Logo = ({ className }) => {
   return (
     <Link to="/" className="flex items-end">
       <img src={logo} alt="Zap Shift Logo" />
-      <h2 className="font-bold text-2xl -ms-2.5">ZapShift</h2>
+      <h2 className={`font-bold text-2xl -ms-2.5 ${className}`}>ZapShift</h2>
     </Link>
   );
 };
